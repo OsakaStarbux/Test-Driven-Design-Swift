9353e62da430881b5840d1984ecea49d5596a2c4
Kevin Bradley <kevin@Kevins-MacBook-Air.local>
2017-07-25 12:18:22 +0900

    Add extensions to UIView for border color/size and corner radius

---
+++ Test Driven Design/MyExtensions.swift
@@ -0,0 +1,42 @@
+//
+//  MyExtensions.swift
+//  Test Driven Design
+//
+//  Created by Kevin Bradley on 2017/07/25.
+//  Copyright © 2017 Kevin Bradley. All rights reserved.
+//
+
+import UIKit
+
+extension UIView {
+    @IBInspectable var cornerRadius: CGFloat {
+        get {
+            return layer.cornerRadius
+        }
+        set {
+            layer.cornerRadius = newValue
+            layer.masksToBounds = newValue > 0
+        }
+    }
+    
+    @IBInspectable var borderWidth: CGFloat {
+        get {
+            return layer.borderWidth
+        }
+        set {
+            layer.borderWidth = newValue
+            layer.masksToBounds = newValue > 0
+        }
+    }
+    
+    @IBInspectable var borderColor: UIColor {
+        get {
+            return UIColor.init(cgColor: layer.borderColor!)
+        }
+        set {
+            layer.borderColor = newValue.cgColor
+        }
+    }
+    
+    
+}
